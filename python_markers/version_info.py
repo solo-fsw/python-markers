e4b74b5ec571539aa6fa8d604bfa9cc726d107ee
JensVanBijs <jensvanbijsterveld@gmail.com>
2023-05-31 16:46:17 +0200

    Updated setup.py information

--- python_markers/version_info.py
+++ python_markers/version_info.py
@@ -1,7 +1,7 @@
 # Metadata for package. Used in setup.py and module file(s).
 
 version = "0.0.1"
-name = "python_markers"
+name = "marker_management"
 description = "A package meant for sending markers using the various digital output devices used at the FSW."
 url = "https://github.com/solo-fsw/python-markers"
 author = "SOLO FSW"
