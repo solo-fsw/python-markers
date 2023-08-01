4e4c2abe372fd07d2c1a5fe6c3103f2bb0665d91
spruitim <i.m.spruit@fsw.leidenuniv.nl>
2023-08-01 13:47:24 +0200

    updated version

--- python_markers/version_info.py
+++ python_markers/version_info.py
@@ -1,6 +1,6 @@
 # Metadata for package. Used in setup.py and module file(s).
 
-version = "0.1.0"
+version = "0.1.1"
 name = "marker_management"
 description = "A package meant for sending markers using the various digital output devices used at the FSW."
 url = "https://github.com/solo-fsw/python-markers"
